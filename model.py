f6fb4ef39a21445eedb62e272f384055a7ca566c
Oreshkin Matway <matveyoreshkin2005gmail.com>
2025-02-18 18:40:41 +0500

    Add init

---
+++ model.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+class MyTreeClf:
+    def __init__(self, 
+                 max_depth: int = 5, 
+                 min_samples_split: int = 2, 
+                 max_leafs: int = 20) -> None:
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.max_leafs = max_leafs
+
+    def __str__(self) -> str:
+        return f'MyTreeClf class: max_depth={self.max_depth}, min_samples_split={self.min_samples_split}, max_leafs={self.max_leafs}'
